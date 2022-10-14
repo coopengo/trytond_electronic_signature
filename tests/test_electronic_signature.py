3422ced783fedef8e74fdf27bc7e0d84ae8ac172
Vincent Garde <67907585+CoopVincent@users.noreply.github.com>
2022-10-14 16:07:14 +0200

    electronic_signature: Update tests to Tryton 6.4 #17
    Fix #00000

--- tests/test_electronic_signature.py
+++ tests/test_electronic_signature.py
@@ -1,8 +1,5 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import unittest
-
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
@@ -11,8 +8,4 @@ class ElectronicSignatureTestCase(ModuleTestCase):
     module = 'electronic_signature'
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            ElectronicSignatureTestCase))
-    return suite
+del ModuleTestCase
